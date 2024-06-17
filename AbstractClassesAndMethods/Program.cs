5018a0fe8a83b5f61bfedc248eda04021a2bf647
amuhammed005 <amuhammed005@st.ug.edu.gh>
2024-06-17 18:40:49 +0000

    This part demonstrates the use of abstract classes and methods in C#.

---
+++ AbstractClassesAndMethods/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace AbstractClassAndMethods
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea()}");
+        }
+    }
+}
+
